e38b66bfa305e07131b2b817919e831ca867e00b
Reid Cameron <reidwesleycameron@gmail.com>
2026-02-02 21:30:43 -0600

    mostly good, just needs keybinds

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -66,6 +66,13 @@ return {
 			"MunifTanjim/nui.nvim",
 			"nvim-tree/nvim-web-devicons", -- optional, but recommended
 		},
+		opts = {
+			filesystem = {
+				filtered_items = {
+					visible = true,
+				},
+			},
+		},
 		lazy = false, -- neo-tree will lazily load itself
 	},
 
